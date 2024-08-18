8ca7a35ad91eacfc46603ac4179601baa632a99a
skldghk <as010394999@gmail.com>
2024-08-18 20:21:44 +0900

    Done :)

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,13 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+// .env 파일의 환경 변수를 로드
+dotenv.config();
 
 export async function connectToDatabase() {
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,  // 기본 포트를 설정할 수도 있습니다.
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
